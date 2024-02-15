cffad8be7ae5e3b88ea210fa05b30802650a3100
Max Kovach <makskovach049@gmail.com>
2024-02-15 19:06:57 +0100

    Добавьте файлы проекта.

---
+++ LinksPairs_Bot/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Channels;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace LinksPairs_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new TelegramBotClient("6940551975:AAG7MajsHho9ANqdQutFi1v1VkXJEDMmoeM");
+            client.StartReceiving(Update, Error);
+            Console.ReadLine();
+        }
+
+        async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+            var message = update.Message;
+            Console.WriteLine($"{message.Chat.FirstName} | {message.Text}");
+            if (message.Text != null && message.Text.ToLower() == "привіт")
+            {
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Привіт");
+                return;
+            }
+
+            if (message.Photo != null)
+            {
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Норм фото, но краще документ!");
+                return;
+            }
+            
+        }
+
+        private static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+        {
+            Console.WriteLine($"Error: {arg2.Message}");
+            return Task.CompletedTask;
+        }
+    }
+}
+
